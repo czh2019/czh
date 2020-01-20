eeb5001ca1a966416e597f6b7b6e892bdd307b12
chenzhihao <369759195@qq.com>
2020-01-20 17:54:15 +0800

    测试提交

---
+++ NetCoreTest/Controllers/ContentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using NetCoreTest.Models;
+
+namespace NetCoreTest.Controllers
+{
+    public class ContentController : Controller
+    {
+        public Content contents;
+        public ContentController(IOptions<Content> option)
+        {
+            contents = option.Value;
+        }
+        public IActionResult Index()
+        {
+            return View(new ContentViewModel { Contents = new List<Content> { contents } });
+        }
+    }
+}
